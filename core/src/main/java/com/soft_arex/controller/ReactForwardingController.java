6ce5a5bd7f8d76e41153335699507937e648f837
wladi <wladislawzel@gmail.com>
2025-05-21 15:51:22 +0300

    fix properties

--- core/src/main/java/com/soft_arex/controller/ReactForwardingController.java
+++ core/src/main/java/com/soft_arex/controller/ReactForwardingController.java
@@ -5,7 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReactForwardingController {
-    @RequestMapping(value = { "/", "/{path:[^\\.]*}" })
+    @RequestMapping(value = {
+            "/",
+            "/login",
+            "/signup",
+            "/edit-profile",
+            "/change-password",
+            "/verify-email",
+            "/questionnaires/**",
+            "/answers/**",
+            "/field"
+    })
     public String forwardToIndex() {
         return "forward:/index.html";
     }
