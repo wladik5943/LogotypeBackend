f4ebc62fb3bc71c28e44c2f28cdbfdbf016d45fa
wladi <wladislawzel@gmail.com>
2025-05-21 10:14:07 +0300

    add forward controller

---
+++ core/src/main/java/com/soft_arex/controller/ReactForwardingController.java
@@ -0,0 +1,12 @@
+package com.soft_arex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactForwardingController {
+    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:^(?!static).*$}"})
+    public String forwardToIndex() {
+        return "forward:/index.html";
+    }
+}
