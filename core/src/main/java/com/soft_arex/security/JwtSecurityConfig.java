006e43aef26bae8f9b47df1b2a84ca2f902c9410
wladi <wladislawzel@gmail.com>
2025-05-21 09:44:25 +0300

    fix spring security 3

--- core/src/main/java/com/soft_arex/security/JwtSecurityConfig.java
+++ core/src/main/java/com/soft_arex/security/JwtSecurityConfig.java
@@ -16,22 +16,15 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-import javax.sql.DataSource;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -78,7 +71,7 @@ public class JwtSecurityConfig {
                                 .requestMatchers("/questionnaires/mine").authenticated()
                                 .requestMatchers("/password/**").permitAll()
                                 .requestMatchers("/user/edit-profile").authenticated()
-                                .requestMatchers(HttpMethod.GET,"/").permitAll()
+                                .anyRequest().permitAll()
 //                        .requestMatchers("/**").permitAll()
                 )
 
