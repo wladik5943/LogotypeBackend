9665f33b52f00fcec3a658c49dc3e332ff0c3221
wladi <wladislawzel@gmail.com>
2025-05-21 16:02:41 +0300

    fix properties

--- core/target/generated-sources/annotations/com/soft_arex/mapper/UserMapperImpl.java
+++ core/target/generated-sources/annotations/com/soft_arex/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-21T15:51:10+0300",
+    date = "2025-05-21T16:02:24+0300",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 @Component
