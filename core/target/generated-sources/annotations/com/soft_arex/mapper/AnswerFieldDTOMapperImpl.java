cba1f215f740d3f57850574d1499743447e72f20
wladi <wladislawzel@gmail.com>
2025-05-21 09:28:17 +0300

    fix front2

--- core/target/generated-sources/annotations/com/soft_arex/mapper/AnswerFieldDTOMapperImpl.java
+++ core/target/generated-sources/annotations/com/soft_arex/mapper/AnswerFieldDTOMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-21T09:01:04+0300",
+    date = "2025-05-21T09:27:54+0300",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 @Component
