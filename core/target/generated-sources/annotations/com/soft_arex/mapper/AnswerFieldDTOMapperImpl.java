afed072a8c8a5f9d6ab15b76dd6c93324f27fca2
wladi <wladislawzel@gmail.com>
2025-05-21 10:32:50 +0300

    add redis

--- core/target/generated-sources/annotations/com/soft_arex/mapper/AnswerFieldDTOMapperImpl.java
+++ core/target/generated-sources/annotations/com/soft_arex/mapper/AnswerFieldDTOMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-21T10:23:20+0300",
+    date = "2025-05-21T10:32:17+0300",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 @Component
